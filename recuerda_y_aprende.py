61467c4af9573d096793ea365f7e17ed63573a74
sandorcan123 <67897605+sandorcan123@users.noreply.github.com>
2024-05-31 02:43:57 -0500

    Update recuerda_y_aprende.py

--- recuerda_y_aprende.py
+++ recuerda_y_aprende.py
@@ -1,4 +1,7 @@
 import random
+import time
+
+record = 0  # Variable para almacenar el récord
 
 print("Recuerda la combinación!!")
 
@@ -8,6 +11,7 @@ while True:
     
     1) Empezar
     2) Salir
+    3) Mostrar récord
     """)
     opcion = int(input("Elige una opción: "))     
 
@@ -17,16 +21,23 @@ while True:
         print("¡Empecemos!")
         numeros_repetidos = [random.randint(0, 9) for _ in range(16)]
         print(numeros_repetidos)
-        recuerda = input("Introduce los números agregados (sin espacios): ")
 
-        # Verificación
+        inicio_tiempo = time.time()
+        recuerda = input("Introduce los números agregados (sin espacios): ")
+        tiempo_transcurrido = time.time() - inicio_tiempo
+        puntos = int(100 / tiempo_transcurrido)  # Calcula los puntos según el tiempo
+
         if recuerda.isdigit() and len(recuerda) == 16:
             recuerda_lista = [int(digito) for digito in recuerda]
             if recuerda_lista == numeros_repetidos:
-                print("¡Felicidades!")
+                print(f"¡Felicidades! Obtuviste {puntos} puntos.")
+                if puntos > record:
+                    record = puntos
             else:
                 print("Vuelve a intentarlo")
         else:
             print("Ingresa 16 dígitos numéricos sin espacios.")
+    elif opcion == 3:
+        print(f"Mejor récord: {record} puntos")
     else:
         print("Opción incorrecta")
